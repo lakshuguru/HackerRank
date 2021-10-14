10bf8b4c1dcd5e6522fb48b99e07fa368fe977ea
LAKSHANAA G S <77288889+lakshuguru@users.noreply.github.com>
2021-10-13 22:45:21 -0700

    Add files via upload

---
+++ Hacker rank challenges/divmod.py
@@ -0,0 +1,27 @@
+'''Task
+Read in two integers,  and , and print three lines.
+The first line is the integer division  (While using Python2 remember to import division from __future__).
+The second line is the result of the modulo operator: .
+The third line prints the divmod of  and .
+
+Input Format
+The first line contains the first integer, , and the second line contains the second integer, .
+
+Output Format
+Print the result as described above.
+
+Sample Input
+
+177
+10
+Sample Output
+
+17
+7
+(17, 7)'''
+
+x=int(input())
+y=int(input())
+print(x//y)
+print(x%y)
+print(divmod(x,y))
